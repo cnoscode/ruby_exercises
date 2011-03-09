52e2d406e6bf25d3d34467cf2041b8d5b2357501
Cyril Jose <cyril_jose@ymail.com>
2011-03-09 13:52:46 -0500

    Unfinished

---
+++ chap10.rb
@@ -0,0 +1,37 @@
+#example in book
+M = 'land'
+o = 'water'
+
+world = [[o,o,o,o,o,o,o,o,o,o,o],
+		 [o,o,o,o,M,M,o,o,o,o,o],
+		 [o,o,o,o,o,o,o,o,M,M,o],
+		 [o,o,o,M,o,o,o,o,o,M,o],
+		 [o,o,o,M,o,M,M,o,o,o,o],
+		 [o,o,o,o,M,M,M,M,o,o,o],
+		 [o,o,o,M,M,M,M,M,M,M,o],
+		 [o,o,o,M,M,o,M,M,M,o,o],
+		 [o,o,o,o,o,o,M,M,o,o,o],
+		 [o,M,o,o,o,M,o,o,o,o,o],
+		 [o,o,o,o,o,o,o,o,o,o,o]]
+		 
+def continent_size world, x, y
+	if world[y][x] != 'land'
+		return 0
+	end
+
+	size = 1
+	world[y][x] = 'counted land'
+	
+	size += continent_size(world, x-1, y-1)
+	size += continent_size(world, x  , y-1)
+	size += continent_size(world, x+1, y-1)
+	size += continent_size(world, x-1, y  )
+	size += continent_size(world, x+1, y  )
+	size += continent_size(world, x-1, y+1)
+	size += continent_size(world, x  , y+1)
+	size += continent_size(world, x+1, y+1)
+	
+	size
+end
+
+puts continent_size(world, 5, 5)
