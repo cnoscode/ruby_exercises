3559b4617932eed8444a5772356230e5306a6ebd
Cyril Jose <cyril_jose@ymail.com>
2011-03-16 10:43:53 -0400

    Finished

--- chap2.rb
+++ chap2.rb
@@ -1,11 +1,11 @@
 print "Hours in a year: "
-puts 24*365
-
+	puts 24*365
+	
 print "Minutes in a decade: "
-puts 60*24*(365*10)
-
+	puts 60*24*(365*10)
+	
 print "My age in seconds: "
-puts 60*60*24*(365*23)
-
+	puts 60*60*24*(365*23)
+	
 print "Author's age: "
-puts 1025000000/(60*60*24*365)
+	puts 1025000000/(60*60*24*365)
