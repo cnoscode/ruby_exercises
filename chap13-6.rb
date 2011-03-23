11a283a6076f2a5766c84454b26e1bf9932e73c4
Cyril Jose <cyril_jose@ymail.com>
2011-03-23 14:43:16 -0400

    orange tree still in progress

--- chap13-6.rb
+++ chap13-6.rb
@@ -2,13 +2,13 @@ class OrangeTree
 	
 	def initialize 
 		@treeHeight = 0
-		@treeAlive = true
 		@num_oranges = 0
+		@treeAlive = true
 	end
 	
 	def height
 		if @treeAlive
-			@heightTree
+			@treeHeight
 		else
 			return "The orange tree is dead!"
 		end
@@ -16,7 +16,9 @@ class OrangeTree
 	
 	def one_year_passes
 		if @treeAlive
-			@treeHeight += 0.5
+			@treeHeight += 2.5
+			#first couple years, no oranges...
+			@num_oranges = 0
 			return "Tree height : #{@treeHeight} feet"
 		else
 			return "Orange tree is dead; that's why it can't grow!"
@@ -25,9 +27,21 @@ class OrangeTree
 	
 	def count_the_oranges
 		if @treeAlive
+			@num_oranges += 4
 			return "Number of oranges : #{@num_oranges}"
 		else
 			return "There are no oranges on the orange tree!"
+		end
+	end
+	
+	def pick_an_orange
+		if @treeAlive
+			if @num_oranges > 0
+				@num_oranges -= 1
+				return "Number of oranges after being picked : #{@num_oranges}"
+			else
+				return "No oranges to pick..."
+			end
 		end
 	end
 
@@ -36,3 +50,7 @@ end # End Class
 orange = OrangeTree.new
 puts orange.one_year_passes
 puts orange.count_the_oranges
+puts orange.one_year_passes
+puts orange.count_the_oranges
+puts orange.pick_an_orange
+puts orange.pick_an_orange
