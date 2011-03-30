5a6d639abc64b33d1cfe02b6b4ac7fc845abd999
Cyril Jose <cyril_jose@ymail.com>
2011-03-30 14:02:00 -0400

    With soft tabs

--- chap9.rb
+++ chap9.rb
@@ -2,17 +2,17 @@ puts "--------------------------------------------------"
 puts "9.5 - Number 1 - Improved Ask method"
 puts "--------------------------------------------------"
 def ask question
-	while true
-		puts question
-		reply =gets.chomp.downcase
-	#if (reply == 'yes' || reply == 'no')
-		if reply == 'yes'
-			return true
-		else
-			return false
-		end
-	break 
-	end
+  while true
+    puts question
+    reply =gets.chomp.downcase
+  #if (reply == 'yes' || reply == 'no')
+    if reply == 'yes'
+      return true
+    else
+      return false
+    end
+  break 
+  end
 end
 
 puts 'Survey...'
@@ -25,18 +25,18 @@ puts "9.5 - Number 2 - Old-school Roman numerals"
 puts "--------------------------------------------------"
 
 def old_roman_numeral num
-	rn = ''
-	#increment for each roman numeral
-	
-	#rn += 'M' * (num % 10000/1000)
-	rn += 'M' * (num/1000)
-	rn += 'D' * (num % 1000/500)
-	rn += 'C' * (num % 500/100)
-	rn += 'L' * (num % 100/50)
-	rn += 'X' * (num % 50/10)
-	rn += 'V' * (num % 10/5)
-	rn += 'I' * (num % 5/1)
-	
+  rn = ''
+  #increment for each roman numeral
+  
+  #rn += 'M' * (num % 10000/1000)
+  rn += 'M' * (num/1000)
+  rn += 'D' * (num % 1000/500)
+  rn += 'C' * (num % 500/100)
+  rn += 'L' * (num % 100/50)
+  rn += 'X' * (num % 50/10)
+  rn += 'V' * (num % 10/5)
+  rn += 'I' * (num % 5/1)
+  
   puts "#{num} is the same as: #{rn}"
 end
 
@@ -58,7 +58,7 @@ ones = (num % 10/1)
     rn += 'CM' 
   elsif hund == 4
     rn += 'CD'
-  else  	
+  else    
     rn += 'D' * (num % 1000/500)
     rn += 'C' * (num % 500/100)
   end
@@ -75,15 +75,15 @@ ones = (num % 10/1)
     # then put 'IX'
     # if ones have a 4 in it
     # then put 'IV'
-	if ones == 9
-		rn += 'IX'
-	elsif ones == 4
-		rn += 'IV'
-	else
-		rn += 'V' * (num % 10/5) 
-		rn += 'I' * (num % 5/1) 
-	end
-	puts "#{num} is the same as: #{rn}"
-	
+  if ones == 9
+    rn += 'IX'
+  elsif ones == 4
+    rn += 'IV'
+  else
+    rn += 'V' * (num % 10/5) 
+    rn += 'I' * (num % 5/1) 
+  end
+  puts "#{num} is the same as: #{rn}"
+  
 end
 mod_roman_numeral(1999)
