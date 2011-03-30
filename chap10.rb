83fdf9d3f7a70e499b4d7f5d554833a909411aad
Cyril Jose <cyril_jose@ymail.com>
2011-03-30 14:04:46 -0400

    With soft tabs

--- chap10.rb
+++ chap10.rb
@@ -3,35 +3,35 @@ M = 'land'
 o = 'water'
 
 world = [[o,o,o,o,o,o,o,o,o,o,o],
-		 [o,o,o,o,M,M,o,o,o,o,o],
-		 [o,o,o,o,o,o,o,o,M,M,o],
-		 [o,o,o,M,o,o,o,o,o,M,o],
-		 [o,o,o,M,o,M,M,o,o,o,o],
-		 [o,o,o,o,M,M,M,M,o,o,o],
-		 [o,o,o,M,M,M,M,M,M,M,o],
-		 [o,o,o,M,M,o,M,M,M,o,o],
-		 [o,o,o,o,o,o,M,M,o,o,o],
-		 [o,M,o,o,o,M,o,o,o,o,o],
-		 [o,o,o,o,o,o,o,o,o,o,o]]
-		 
+     [o,o,o,o,M,M,o,o,o,o,o],
+     [o,o,o,o,o,o,o,o,M,M,o],
+     [o,o,o,M,o,o,o,o,o,M,o],
+     [o,o,o,M,o,M,M,o,o,o,o],
+     [o,o,o,o,M,M,M,M,o,o,o],
+     [o,o,o,M,M,M,M,M,M,M,o],
+     [o,o,o,M,M,o,M,M,M,o,o],
+     [o,o,o,o,o,o,M,M,o,o,o],
+     [o,M,o,o,o,M,o,o,o,o,o],
+     [o,o,o,o,o,o,o,o,o,o,o]]
+     
 def continent_size world, x, y
-	if world[y][x] != 'land'
-		return 0
-	end
+  if world[y][x] != 'land'
+    return 0
+  end
 
-	size = 1
-	world[y][x] = 'counted land'
-	
-	size += continent_size(world, x-1, y-1)
-	size += continent_size(world, x  , y-1)
-	size += continent_size(world, x+1, y-1)
-	size += continent_size(world, x-1, y  )
-	size += continent_size(world, x+1, y  )
-	size += continent_size(world, x-1, y+1)
-	size += continent_size(world, x  , y+1)
-	size += continent_size(world, x+1, y+1)
-	
-	size
+  size = 1
+  world[y][x] = 'counted land'
+  
+  size += continent_size(world, x-1, y-1)
+  size += continent_size(world, x  , y-1)
+  size += continent_size(world, x+1, y-1)
+  size += continent_size(world, x-1, y  )
+  size += continent_size(world, x+1, y  )
+  size += continent_size(world, x-1, y+1)
+  size += continent_size(world, x  , y+1)
+  size += continent_size(world, x+1, y+1)
+  
+  size
 end
 
 puts continent_size(world, 5, 5)
@@ -45,36 +45,36 @@ puts "--------------------------------------------------"
 
 #takes in elements of array
 def sort a
-	rec_sort a, []
+  rec_sort a, []
 end
 
 #sorted_a starts out as empty array
 
 def rec_sort unsorted_a, sorted_a
-	if unsorted_a.length <= 0
-		#once unsorted_a is empty, return contents of sorted_a 
-		return sorted_a
-	end
-	
-	#take last element of array and compare it to the others 
-	
-	smallest = unsorted_a.pop
-	still_unsorted_a = []		
-	
-	unsorted_a.each do |unsorted_elem|
-		if unsorted_elem < smallest
-			still_unsorted_a.push smallest
-			smallest = unsorted_elem
-			#puts sorted_a
-		else
-			still_unsorted_a.push unsorted_elem		
-		end
-	end
-	
-	sorted_a.push smallest
-	
-	rec_sort still_unsorted_a, sorted_a
-	
+  if unsorted_a.length <= 0
+    #once unsorted_a is empty, return contents of sorted_a 
+    return sorted_a
+  end
+  
+  #take last element of array and compare it to the others 
+  
+  smallest = unsorted_a.pop
+  still_unsorted_a = []   
+  
+  unsorted_a.each do |unsorted_elem|
+    if unsorted_elem < smallest
+      still_unsorted_a.push smallest
+      smallest = unsorted_elem
+      #puts sorted_a
+    else
+      still_unsorted_a.push unsorted_elem   
+    end
+  end
+  
+  sorted_a.push smallest
+  
+  rec_sort still_unsorted_a, sorted_a
+  
 end
 
 puts sort(['be', 'ab', 'do', 'cj'])
@@ -83,9 +83,9 @@ puts "10.3 - Number 1 -Shuffle"
 puts "--------------------------------------------------"
 
 def shuffle a
-	a_one = []
-	a_one = a.sort_by{rand} 
-	puts a_one
+  a_one = []
+  a_one = a.sort_by{rand} 
+  puts a_one
 end
 
 shuffle(['be', 'ab', 'do', 'cj'])
@@ -96,33 +96,33 @@ puts "--------------------------------------------------"
 
 #used code from problem "Sort Array"
 def dict_sort a
-	recDict_sort a, []
+  recDict_sort a, []
 end
 
 def recDict_sort unsorted_a, sorted_a
-	if unsorted_a.length <= 0
-		#once unsorted_a is empty, return contents of sorted_a 
-		return sorted_a
-	end
-	
-	#take last element of array and compare it to the others 
-	
-	smallest = unsorted_a.pop
-	still_unsorted_a = []		
-	
-	unsorted_a.each do |unsorted_elem|
-		if unsorted_elem.downcase < smallest.downcase
-			still_unsorted_a.push smallest
-			smallest = unsorted_elem
-			#puts sorted_a
-		else
-			still_unsorted_a.push unsorted_elem		
-		end
-	end
-	
-	sorted_a.push smallest
-	
-	recDict_sort still_unsorted_a, sorted_a
-	
+  if unsorted_a.length <= 0
+    #once unsorted_a is empty, return contents of sorted_a 
+    return sorted_a
+  end
+  
+  #take last element of array and compare it to the others 
+  
+  smallest = unsorted_a.pop
+  still_unsorted_a = []   
+  
+  unsorted_a.each do |unsorted_elem|
+    if unsorted_elem.downcase < smallest.downcase
+      still_unsorted_a.push smallest
+      smallest = unsorted_elem
+      #puts sorted_a
+    else
+      still_unsorted_a.push unsorted_elem   
+    end
+  end
+  
+  sorted_a.push smallest
+  
+  recDict_sort still_unsorted_a, sorted_a
+  
 end
 puts dict_sort(['Hello', 'ab', 'do', 'run'])
