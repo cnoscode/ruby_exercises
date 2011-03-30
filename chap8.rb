f9cc080674c46237375b43a39deae2ef9881c37e
Cyril Jose <cyril_jose@ymail.com>
2011-03-30 13:55:20 -0400

    Test with soft tabs

--- chap8.rb
+++ chap8.rb
@@ -1,18 +1,17 @@
-
 puts "------------------------------------------"
 puts "8.3 - Number 1 - Sort Array on User Input"
 puts "------------------------------------------"
-puts "Please enter a bunch of words:"
+puts "Please enter a bunch of words(press enter when you are finished):"
 
 arr = []
 
-	while true
-		word = gets.chomp 
-		if word == ''
-			break
-		end
-	arr.push word
-	end
+while true
+  word = gets.chomp 
+  if word == ''
+    break
+  end
+arr.push word
+end
 
 puts "Sorted:"
 puts arr.sort
@@ -27,15 +26,15 @@ arr =	[['Getting Started',  1],
 		['Letters',		     13]]
 
 header = 'Table of Contents'
-	puts header.center(50)
-	puts
+puts header.center(50)
+puts
 
 ch_num = 1
 arr.each do |elem|
-	ch_name = elem[0]
-	page = elem[1]
-	#puts "Chapter #{ch_num}:"
-	puts 'Chapter ' + ch_num.to_s + ': ' + ch_name.ljust(25) + 'page '.rjust(13) + page.to_s.rjust(0)
-	ch_num += 1
+  ch_name = elem[0]
+  page = elem[1]
+  #puts "Chapter #{ch_num}:"
+  puts 'Chapter ' + ch_num.to_s + ': ' + ch_name.ljust(25) + 
+  'page '.rjust(13) + page.to_s.rjust(0)
+  ch_num += 1
 end
-puts
