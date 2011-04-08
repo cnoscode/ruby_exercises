f1a6922207cadaea59e7be84e293ffe9e04a984c
Cyril Jose <cyril_jose@ymail.com>
2011-04-08 12:25:01 -0400

    Fied newline problem and added command line args and file format checks

--- fastaParser.rb
+++ fastaParser.rb
@@ -1,9 +1,18 @@
-def search_fasta query
+query = ARGV[0]
+input_file = ARGV[1]
+output_file = ARGV[2]
+
+unless ARGV.length == 3 && input_file.downcase[-6..-1] == '.fasta'
+  puts "Error => exception: ruby testParse.rb <query> <your_fasta_file.fasta> <your_output.txt>"
+  puts "                    Fasta file must be in '.fasta' format."
+  exit
+end
+
+
+def search_fasta( query, input_file, output_file ) 
   
- # query = ARGV[1]
-  
-  input_file = File.open("/Users/cjose/work/ruby_exercises/test.fa", "r" )
-  char_array = input_file.each_char.to_a
+  open_fasta = File.open( input_file, "r" )
+  char_array = open_fasta.each_char.to_a
   
   unless char_array[0] =~ />/
     puts "Error: Please input a FASTA file."
@@ -12,17 +21,21 @@ def search_fasta query
   
   input_file = char_array.to_s.chomp
   seqs_in_array = input_file.split("\n\n")
-  results_file = File.new("/Users/cjose/work/ruby_exercises/search_results.txt", "w")
+  
+  results_file = File.open( output_file, "w" )
   hits_counter = 0
   
   seqs_in_array.each do |each_entry|
-  # each_entry.delete!("\n")
-    if each_entry =~ /#{query}/i
+    if each_entry.sub(/\n/, '') =~ /#{query}/i
       results_file.puts "Search hit #{hits_counter += 1} :"
       results_file.puts each_entry
     end
   end
 puts "Sequences that match query: #{hits_counter}"
+
+open_fasta.close
+results_file.close
 end
-search_fasta("CGATGCGTG")
-# CTGAAGTTAAATTTAATAAAAAAAAAAAAAAAAA
+
+
+search_fasta( query, input_file, output_file )
